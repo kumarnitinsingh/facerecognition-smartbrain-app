7f023c0648cd0f0b676d56c91782f4a3c007edcd
kumarnitinsingh <nitin962141@gmail.com>
2020-05-30 13:32:13 +0530

    Updates

---
+++ precache-manifest.a6e8f9058df650d849ede279544b708e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d001615a4121aa5f3aca1842dec346",
+    "url": "/facerecognition-smartbrain-app/index.html"
+  },
+  {
+    "revision": "dd58e629450e3224b030",
+    "url": "/facerecognition-smartbrain-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "99e0279305a2096a9359",
+    "url": "/facerecognition-smartbrain-app/static/css/main.dca363e1.chunk.css"
+  },
+  {
+    "revision": "dd58e629450e3224b030",
+    "url": "/facerecognition-smartbrain-app/static/js/2.c8756b25.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognition-smartbrain-app/static/js/2.c8756b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e0279305a2096a9359",
+    "url": "/facerecognition-smartbrain-app/static/js/main.f18a7a18.chunk.js"
+  },
+  {
+    "revision": "0760419bfc82db0ad721",
+    "url": "/facerecognition-smartbrain-app/static/js/runtime-main.c4e2fe3b.js"
+  }
+]);
